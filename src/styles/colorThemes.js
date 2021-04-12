4cc44b5573108c94b9bfe980a8e2fef52483e237
veeepi <vpreduna90@gmail.com>
2021-04-11 19:55:50 -0700

    styling: defined color theme "holisticTheme"; #13

---
+++ src/styles/colorThemes.js
@@ -0,0 +1,19 @@
+
+export const holisticTheme = {
+    // Root
+    bgRoot: "#5C4E75",
+    textRoot: '#EFEFEF',
+    // Container
+    bgContainer: '#A395B8',
+    // Text Inputs
+    bgInput: '#C4C4C4',
+    textLabel: '#EFEFEF',
+    textInput: '#000000',
+    // Buttons - primary
+    bgBtnPrimary: '#AAB895',
+    textBtnPrimary: '#4E755C',
+    // Buttons - secondary
+    bgBtnSecondary: '#C8D1BB',
+    textBtnSecondary: '#4E755C',
+
+}
