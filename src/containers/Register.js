40d9228273ff7f2cb0cad9b7a1b94bee00387f4f
veeepi <vpreduna90@gmail.com>
2021-04-12 01:06:14 -0700

    +Register, Tabs for user types; user model; #10

--- src/containers/Register.js
+++ src/containers/Register.js
@@ -1,9 +1,83 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react';
+import Typography from '@material-ui/core/Typography';
+import AppBar from '@material-ui/core/AppBar';
+import Tabs from '@material-ui/core/Tabs';
+import Tab from '@material-ui/core/Tab';
+import Input from '@material-ui/core/Input';
+import InputLabel from '@material-ui/core/InputLabel';
+import FormControl from '@material-ui/core/FormControl';
+import Button from '@material-ui/core/Button';
+import InputAdornment from '@material-ui/core/InputAdornment';
+import AccountCircle from '@material-ui/icons/AccountCircle';
+import VpnKeyIcon from '@material-ui/icons/VpnKey';
+import TabPanel from '../containers/TabPanel';
+import authStyles from '../styles/authStyles';
+import RegisterClientForm from '../components/forms/RegisterClientForm';
+import RegisterCoachForm from '../components/forms/RegisterCoachForm';
+
 
 export default function Register() {
+    const classes = authStyles();
+
+    const [typeValue, setTypeValue] = React.useState(0);
+    const [error, setError] =  useState('');
+    const [user, setUser] = useState({
+        // common props (client and coach)
+        username: '', // required
+        email: '', // required
+        password: '', // required
+        confirmPassword: '', // required
+        firstName: '',
+        lastName: '',
+        address: '',
+        city: '',
+        province: '',
+        country: '',
+        phoneNumber: '',
+        emergencyContactName: '', // client only
+        emergencyContactPhone: '', // client only
+        healthGoals: '', // client only
+        healthIssues: '', // client only
+    }) // final user object to submit
+
+    const handleTypeTabChange = (event, newValue) => {
+        setTypeValue(newValue)
+    }
+
+    const handleRegisterClient = (user) => {
+        // e.preventDefault();
+        console.log("Register clicked", user)
+    }
+    const handleRegisterCoach = (user) => {
+        // e.preventDefault();
+        console.log("Register clicked", user)
+    }
+
+
     return (
         <div>
-            <h1>Register</h1>
+            <AppBar className={classes.registerAppBar} position="static">
+                <Typography>Are you a client or a coach?</Typography>
+                <Tabs
+                    className={classes.authTabs}
+                    value={typeValue}
+                    onChange={handleTypeTabChange}
+                    // indicatorColor="primary"
+                    // textColor="primary"
+                    centered
+                >
+                    <Tab label="Client" />
+                    <Tab label="Coach" />
+                </Tabs>
+            </AppBar>
+
+            <TabPanel value={typeValue} index={0}>
+                <RegisterClientForm onSubmit={handleRegisterClient} user={user} setUser={setUser} />
+            </TabPanel>
+            <TabPanel value={typeValue} index={1}>
+                {/* <RegisterCoachForm onSubmit={handleRegisterCoach} user={user} setUser={setUser} /> */}
+            </TabPanel>
+
         </div>
     )
 }
