16e88e5afe9c52d0f0ef6f8a46670b6d04d8ae93
veeepi <vpreduna90@gmail.com>
2021-04-13 00:10:51 -0700

    +NavLinks to AppBar; #27

--- src/containers/MenuAppBar.js
+++ src/containers/MenuAppBar.js
@@ -1,39 +1,22 @@
 import React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
+import { Link } from 'react-router-dom';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import Typography from '@material-ui/core/Typography';
 import IconButton from '@material-ui/core/IconButton';
 import MenuIcon from '@material-ui/icons/Menu';
 import AccountCircle from '@material-ui/icons/AccountCircle';
-import Switch from '@material-ui/core/Switch';
-import FormControlLabel from '@material-ui/core/FormControlLabel';
-import FormGroup from '@material-ui/core/FormGroup';
 import MenuItem from '@material-ui/core/MenuItem';
 import Menu from '@material-ui/core/Menu';
 import { userSignOut } from '../firebase/services'; 
+import navbarStyles from '../styles/navbarStyles';
 
-const useStyles = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-  },
-  menuButton: {
-    marginRight: theme.spacing(2),
-  },
-  title: {
-    flexGrow: 1,
-  },
-}));
+export default function MenuAppBar({user}) {
+  const classes = navbarStyles();
 
-export default function MenuAppBar() {
-  const classes = useStyles();
-  const [auth, setAuth] = React.useState(true);
   const [anchorEl, setAnchorEl] = React.useState(null);
   const open = Boolean(anchorEl);
-
-  const handleChange = (event) => {
-    setAuth(event.target.checked);
-  };
 
   const handleMenu = (event) => {
     setAnchorEl(event.currentTarget);
@@ -44,22 +27,12 @@ export default function MenuAppBar() {
   };
 
   return (
-    <div className={classes.root}>
-      <FormGroup>
-        <FormControlLabel
-          control={<Switch checked={auth} onChange={handleChange} aria-label="login switch" />}
-          label={auth ? 'Logout' : 'Login'}
-        />
-      </FormGroup>
       <AppBar position="static">
-        <Toolbar>
-          <IconButton edge="start" className={classes.menuButton} color="inherit" aria-label="menu">
-            <MenuIcon />
-          </IconButton>
-          <Typography variant="h6" className={classes.title}>
-            Photos
-          </Typography>
-          {auth && (
+        <Toolbar className={classes.appBarContainer}>
+          <Typography className={classes.appBarTitle}>Holistic</Typography>
+          <Link className={classes.appBarNavLink} to="/feed">Feed</Link>
+          <Link className={classes.appBarNavLink} to="/dash">Dash</Link>
+          {user && (
             <div>
               <IconButton
                 aria-label="account of current user"
@@ -85,12 +58,12 @@ export default function MenuAppBar() {
                 open={open}
                 onClose={handleClose}
               >
+                <MenuItem><Link className={classes.menuItem} to="/dash">To Dash</Link></MenuItem>
                 <MenuItem onClick={() => userSignOut()}>SignOut</MenuItem>
               </Menu>
             </div>
           )}
         </Toolbar>
       </AppBar>
-    </div>
   );
 }
