1adc2f0a60d1f3ba107967ee021e62358f636c66
veeepi <vpreduna90@gmail.com>
2021-04-15 22:24:24 -0700

    Publish Session function updates status; #50

--- src/containers/SessionDetails.js
+++ src/containers/SessionDetails.js
@@ -11,7 +11,10 @@ export default function SessionDetails({authUser, dataUser, session, exitSession
     const classes = sessionDetailStyles();
 
     // Initialize - GET Session Actions 
+    const usersRef = firebase.firestore().collection('users');
     const actionsRef = firebase.firestore().collection('actions')
+    const sessionsRef = firebase.firestore().collection('sessions');
+
     const [actions, setActions] = useState([])
     useEffect(() => {
         actionsRef
@@ -34,7 +37,22 @@ export default function SessionDetails({authUser, dataUser, session, exitSession
     const [sessionInProgress, setSessionInProgress] = useState(session.status)
     const startSession = () => {
         setSessionInProgress(true)
+        // 
         console.log("startSession clicked")
+    }
+    const publishSession = () => {
+        // update Session status to 'upcoming'
+        sessionsRef.doc(session.id).update({
+            status: 'upcoming',
+        })
+        // update owner's draftSession to ""
+        usersRef.doc(authUser.uid).update({
+            draftSessionId: "",
+        })
+        // update participant's sessions array
+        usersRef.doc(session.participantUserId).update({
+            sessions: firebase.firestore.FieldValue.arrayUnion(session.id),
+        })
     }
     const completeSession = () => {
         setSessionInProgress(false)
@@ -55,13 +73,14 @@ export default function SessionDetails({authUser, dataUser, session, exitSession
             <Box className={classes.sessionDetails}>
                 <Box className={classes.sessionInfo}>
                     <Typography>{session.name}</Typography>
-                    <Typography>{'Coach: ' + session.coachName}</Typography>
+                    <Typography>{'Coach: ' + session.coachUsername}</Typography>
                     <Typography>{'Participant: ' + session.participantUsername}</Typography> 
                     <Typography>{session.notes}</Typography>
 
                 </Box>
                 
                 <Box className={classes.sessionButtons}>
+                    <Button className={classes.buttonPrimary} onClick={() => publishSession()}>Publish Session</Button>
                     <Button className={classes.buttonPrimary} onClick={() => startSession()}>Start Session</Button>
                 </Box>
 
@@ -85,7 +104,9 @@ export default function SessionDetails({authUser, dataUser, session, exitSession
             { addingAction && <NewActionForm action={actions[actions.length-1]} authUser={authUser} dataUser={dataUser} toggleAddAction={toggleAddAction} />}
 
             <Box className={classes.sessionActionsButtions}>
-                <Button className={classes.buttonPrimary} onClick={() => toggleAddAction()}>{addingAction ? 'Cancel Add Set' : 'Add Set'}</Button>
+                {   session.status &&
+                    <Button className={classes.buttonPrimary} onClick={() => toggleAddAction()}>{addingAction ? 'Cancel Add Set' : 'Add Set'}</Button>
+                }
                 <Button className={classes.buttonPrimary} onClick={() => completeSession()}>COMPLETE SESSION</Button>
             </Box>
                 
